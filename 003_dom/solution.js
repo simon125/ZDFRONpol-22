516c667a0b5cffc6635ea7a7f5f19405ed0ede5b
Szymon Oleszek <szymqwerty@gmail.com>
2023-03-25 02:59:46 +0100

    dom & events

---
+++ 003_dom/solution.js
@@ -0,0 +1,195 @@
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
+ *
+ * Document Object Model (DOM) - przeglądarka interpretuje kod HTML i dostarcza obiekt, który JS jest w stanie odczytywać i modyfikować
+ *
+ * najprościej mówiąc DOM dostarcza nam obiekt który nazywa się "document" i ten obiekt ma bardzo bardzo bardzo dużo
+ * różnych pól i metod które pozwalają na interakcje z HTMLem
+ *
+ */
+
+/**
+ * Tworzenie elementu
+ *
+ * 1) createElement
+ *
+ * 2) template string (backticki - znaki pojedynczego cudzysłowia pod klawiszem esc) + właściwości/pola innerHTML/innerText
+ *
+ */
+
+/**
+ * Osadzanie nowostworzonego elementu w elemncie body
+ */
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/write
+// 1) document.write ❌
+
+// 2) document.body... ✅
+
+/**
+ * Pobieranie elementów z dokumentu HTML
+ */
+
+// 1) metoda querySelector
+
+// 2) metoda getElementById
+
+// 3) metoda getElementsByClassName
+
+/**
+ * Zadanie
+ *
+ * stwórz dowolny element htmlowy w pliku index.html
+ * dodaj mu potrzebne atrubuty i spróbuj go pobrać nowo poznanymi Ci metodami
+ * wynik przypisz do zmiennej po czym wykonsoluj zmienną z pobranym element (console.log)
+ */
+
+/**
+ * manipulowanie classami - lista klas jest dostępna pod polem classList
+ * dodatkowo można manipulować tymi klasami przy pomocy szerokiej gamy metod dostępnej też pod tym polem
+ */
+
+/**
+ * bezpośrednie stylowanie elementu
+ * element.style = '';
+ */
+
+/**
+ * Wstawianie elementu / osadzanie w dowolnym miejscu w dokumencie
+ */
+
+/**
+ * Ustawianie/setowanie atrubutów na przykładzie checkboxa
+ */
+
+/**
+ * Usuwanie elementu - metoda remove dostępna na elemencie HTML/Nodzie
+ */
+
+/**
+ * Zadanie
+ *
+ * Stwórz w JSie element html div który będzie miał 100px szerokości i 100px wysokości
+ * oraz dowolny kolor
+ *
+ * niech ten div ma w sobie zagnieżdzony kolejny div który będzie miał 50% szerokości i wyskokści oraz inny kolor od rodzica
+ *
+ * zakoduj też następujący scenariusz:
+ *
+ * niech:
+ *  po 3 sekundach element ten zwiększy swoją wysokość i szerokosc o 100px na każdą długość
+ *  po 8 sekundach usuń element
+ *
+ */
+
+const div = document.createElement("div");
+
+div.style.height = "100px";
+div.style.width = "100px";
+div.style.backgroundColor = "blue";
+
+div.innerHTML = `<div style="height: 50%; width: 50%; background-color: red;"></div>`;
+
+document.body.appendChild(div);
+
+setTimeout(() => {
+  div.style.height = "200px";
+  div.style.width = "200px";
+}, 3000);
+
+setTimeout(() => {
+  div.remove();
+}, 8000);
+
+/**
+ * Zadanie
+ *
+ * Przekopiuj jedno z zadań z poprzedniego modułu (002_html_in_js) któro wymagało stworzenia struktury HTML w zmiennej
+ * i umieść tą strukturę w wybranym miejscu na stronie za pomocą pola innerHTML
+ *
+ *
+ * możesz stworzyć dodatkowy element w pliku HTML lub w JSie
+ * i dopiero osadzić przekopiowaną strukture HTML
+ *
+ * experymentuj a w razie coś pytaj😀
+ *
+ */
+
+const users = [
+  { name: "Jane", lastName: "Doe", age: 20 },
+  { name: "Jack", lastName: "Jake", age: 15 },
+  { name: "John", lastName: "Doe", age: 30 },
+];
+
+let usersTable = "";
+
+users.forEach((user, index) => {
+  /**
+   * Dodawanie/dobudowywanie wierszy HTMLowych na podstawie
+   * elementów tablicy
+   */
+  usersTable =
+    usersTable +
+    `<tr>
+          <td>${user.name}</td>
+          <td>${user.lastName}</td>
+          <td>${user.age}</td>
+      </tr>`;
+
+  if (index === users.length - 1) {
+    /**
+     * Po ostatnim wierszu owrapowywujemy (otaczamy) wiersze tabelki (<tr></tr>)
+     * pozostałą wymaganą strukturą HTMLową
+     */
+
+    usersTable = `<table>
+          <tbody>${usersTable}</tbody>
+          </table>`;
+  }
+});
+
+const div1 = document.createElement("div");
+
+div1.innerHTML = usersTable;
+
+document.body.appendChild(div1);
+
+/**
+ * Zadanie ⭐⭐
+ *
+ * pobierz element po id-ku znaną Ci metodą i przypisz go do zmiennej o dowolnej nazwie (sugestia: flexContainer)
+ *
+ * ustaw interwał czasowy (setInterval) który będzie co 2 sekundy wybierał z tablicy klas randomową klasę i ustawiał ją
+ * jako klase elementu flexContainer pamiętaj o usunięciu klasy z poprzedniego wywołania
+ *
+ * do wylosowania randomowej liczy z przedziału możesz zaimplementować funkcje bądź skorzystać z dobrodziejstw internetu
+ * https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+ *
+ * jeżeli chcesz gotowca to patrz hint3
+ *
+ * hint1: przypisz klase bezpośrednio do pola classList
+ * hint2: żeby pominąć pisanie nie potrzebnej logiki sprawdzającej jakie klasy są w danym elemencie możesz przypisać
+ * do classList wartości randomowej plus "flex-container" która była na początku i jest potrzebna do tego żeby element dalej
+ * był flex kontenerem
+ *
+ * hint3: do wylosowania randomowej liczby z przedziału możesz użyć funkcji poniżej i wywoływać ją w interwale z parametrami 0 oraz 2
+ *
+ * function randomIntFromInterval(min, max) {
+ *  return Math.floor(Math.random() * (max - min + 1) + min)
+ * }
+ *
+ */
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const classes = ["start", "center", "end"];
+
+const flexContainer = document.getElementById("flexContainer");
+
+setInterval(() => {
+  const className = classes[randomIntFromInterval(0, 2)];
+
+  flexContainer.classList = `flex-container ${className}`;
+}, 2000);
